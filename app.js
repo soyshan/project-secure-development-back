533a4990fb04ce0789f3c0e396b618295e98fca7
soyshan <shanbu131@gmail.com>
2024-04-24 18:26:09 +0200

    fix: add an variable to port in app

--- app.js
+++ app.js
@@ -40,7 +40,10 @@ app.use('/uploads',express.static('uploads'));
 
 app.use('/users', userRoutes);
 
-app.listen(8000, ()=>{
-    console.log('Server UP running in http://localhost:8000/')
-})
+const port = process.env.PORT ;
+
+app.listen(port, () => {
+    console.log(`Server UP running in http://localhost:${port}/`);
+});
+
 
